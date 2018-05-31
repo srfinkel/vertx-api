a4ae7a22decc13a58da5e501e056811f74fdb374
Sarah Ruth Finkel <srfinkel@gmail.com>
2018-05-31 00:51:03 -0600

    updated JobsAPIVerticle

--- jobsapi/src/main/java/com/srfinkel/jobsapi/JobsAPIVerticle.java
+++ jobsapi/src/main/java/com/srfinkel/jobsapi/JobsAPIVerticle.java
@@ -1,13 +1,28 @@
 package com.srfinkel.jobsapi;
 
-/**
- * Hello world!
- *
- */
-public class JobsAPIVerticle 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
+public class JobsAPIVerticle extends AbstractVerticle {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(JobsAPIVerticle.class);
+
+	public static void main( String[] args ) {
+		Vertx vertx = Vertx.vertx();
+
+	}
+	
+	@Override
+    public void start() {
+    	LOGGER.info("Verticle JobsAPIVerticle Started");
+	
+	}
+    	
+	@Override
+	public void stop() {
+		LOGGER.info("Verticle JobsAPIVerticle Stopped");
+	}
+
 }
