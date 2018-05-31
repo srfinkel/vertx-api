1d783a6d3e8c1c3e2b4411252560289c38217091
Sarah Ruth Finkel <srfinkel@gmail.com>
2018-05-31 00:42:13 -0600

    Added java files

---
+++ jobsapi/src/main/java/com/srfinkel/jobsapi/JobsAPIVerticle.java
@@ -0,0 +1,13 @@
+package com.srfinkel.jobsapi;
+
+/**
+ * Hello world!
+ *
+ */
+public class JobsAPIVerticle 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
