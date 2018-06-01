ee6de30d3e1af3208daf245714060698c2a36c22
Sarah Ruth Finkel <srfinkel@gmail.com>
2018-06-01 00:20:30 -0600

    Fixed dependencies in pom.xml; Added router object to main Verticle

--- jobsapi/src/main/java/com/srfinkel/jobsapi/JobsAPIVerticle.java
+++ jobsapi/src/main/java/com/srfinkel/jobsapi/JobsAPIVerticle.java
@@ -5,6 +5,9 @@ import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+
 
 public class JobsAPIVerticle extends AbstractVerticle {
 
@@ -14,19 +17,25 @@ public class JobsAPIVerticle extends AbstractVerticle {
 		Vertx vertx = Vertx.vertx();
 		vertx.deployVerticle(new JobsAPIVerticle());
 	}
-	
+
 	@Override
 	public void start(Future<Void> fut) {
 		LOGGER.info("Verticle JobsAPIVerticle Started");
 
-		vertx.createHttpServer().requestHandler(r -> {
-			r.response().end("<h1>Hello Updated this Verticle</h1>");
-		}).listen(config().getInteger("http.port", 8080), result -> {
-			if (result.succeeded()) {
-				fut.complete();
-			} else {
-				fut.fail(result.cause());
-			}
+		Router router = Router.router(vertx);
+
+		router.route("/").handler(routingContext -> {
+			HttpServerResponse response = routingContext.response();
+			response.putHeader("content-type", "text/html").end("<h1>Rest with Vertx</h1>");
 		});
+
+		vertx.createHttpServer().requestHandler(router::accept).listen(config().getInteger("http.port", 8080),
+				result -> {
+					if (result.succeeded()) {
+						fut.complete();
+					} else {
+						fut.fail(result.cause());
+					}
+				});
 	}
 }
