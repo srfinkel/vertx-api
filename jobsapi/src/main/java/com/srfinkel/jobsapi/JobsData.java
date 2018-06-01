05b519065a1554eaec992779507ecd80738f4e84
Sarah Ruth Finkel <srfinkel@gmail.com>
2018-06-01 07:53:50 -0600

    Started the constructor for JobsData

--- jobsapi/src/main/java/com/srfinkel/jobsapi/JobsData.java
+++ jobsapi/src/main/java/com/srfinkel/jobsapi/JobsData.java
@@ -1,10 +1,29 @@
 package com.srfinkel.jobsapi;
 
+import java.util.Date;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class JobsData {
 
 	  private static final AtomicInteger COUNTER = new AtomicInteger();
 	  
+	  private final int id;
+	  private String title;
+	  private String keyword;
+	  private Date date;
+	  private String rate;
+	  private String country;
+	  private String language;
 	  
+	  boolean fulltime;
+	  boolean parttime;
+	  
+	  public JobsData(String title, String keyword) {
+		  this.id = COUNTER.getAndIncrement();
+		  this.title = title;
+		  this.keyword = keyword;
+		  this.date = date;
+		  this.fulltime = false;
+		  this.parttime = false;	  
+	  }
 }
