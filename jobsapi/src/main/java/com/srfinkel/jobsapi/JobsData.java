bc02a0c27f4b0d31d58cc9e866427a0e142376da
Sarah Ruth Finkel <srfinkel@gmail.com>
2018-06-01 00:44:06 -0600

    Added JobsData class

---
+++ jobsapi/src/main/java/com/srfinkel/jobsapi/JobsData.java
@@ -0,0 +1,10 @@
+package com.srfinkel.jobsapi;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class JobsData {
+
+	  private static final AtomicInteger COUNTER = new AtomicInteger();
+	  
+	  
+}
