54336f733a19f31f547f7fac81ad22c42dc4a876
Sarah Ruth Finkel <srfinkel@gmail.com>
2018-05-31 19:28:35 -0600

    Deleted stop method as the garbage is handled by vertx; Updated the requestHandler; Deleted main

--- jobsapi/src/main/java/com/srfinkel/jobsapi/JobsAPIVerticle.java
+++ jobsapi/src/main/java/com/srfinkel/jobsapi/JobsAPIVerticle.java
@@ -1,7 +1,6 @@
 package com.srfinkel.jobsapi;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Vertx;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.core.Future;
@@ -10,27 +9,18 @@ public class JobsAPIVerticle extends AbstractVerticle {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(JobsAPIVerticle.class);
 
-	public static void main( String[] args ) {
-		
-		Vertx vertx = Vertx.vertx();
-		
-		vertx.deployVerticle(new JobsAPIVerticle());
-
-	}
-	
 	@Override
-    public void start(Future<Void> fut) {
-    	LOGGER.info("Verticle JobsAPIVerticle Started");
-    	
-//    	vertx.createHttpServer().requestHandler(request -> {
-//    		request.response().end("<h1>Jobs Working</h1>");	
-//    		}).listen(8080);
-	
-	}
-    	
-	@Override
-	public void stop() {
-		LOGGER.info("Verticle JobsAPIVerticle Stopped");
-	}
+	public void start(Future<Void> fut) {
+		LOGGER.info("Verticle JobsAPIVerticle Started");
 
+		vertx.createHttpServer().requestHandler(r -> {
+			r.response().end("<h1>Updated this Verticle</h1>");
+		}).listen(config().getInteger("http.port", 8080), result -> {
+			if (result.succeeded()) {
+				fut.complete();
+			} else {
+				fut.fail(result.cause());
+			}
+		});
+	}
 }
