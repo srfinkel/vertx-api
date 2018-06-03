b19ecd627dd9cd4a18be168db4c86702c4ac02b8
Sarah Ruth Finkel <srfinkel@gmail.com>
2018-06-02 19:55:50 -0600

    configured Kafka consumer to subscribe to jobs topic

--- jobsapi/src/main/java/com/srfinkel/jobsapi/JobsKafkaConsumer.java
+++ jobsapi/src/main/java/com/srfinkel/jobsapi/JobsKafkaConsumer.java
@@ -1,5 +1,36 @@
 package com.srfinkel.jobsapi;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import io.vertx.core.Vertx;
+import io.vertx.kafka.client.consumer.KafkaConsumer;
+
 public class JobsKafkaConsumer {
 
+	public void jobsConsumer(Vertx vertx) {
+
+		Map<String, String> config = new HashMap<>();
+		config.put("bootstrap.servers", "localhost:9092");
+		config.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		config.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		config.put("group.id", "jobsCounter");
+		config.put("auto.offset.reset", "earliest");
+		config.put("enable.auto.commit", "false");
+
+		KafkaConsumer<String, String> consumer = KafkaConsumer.create(vertx, config);
+
+		consumer.handler(record -> {
+			System.out.println("Processing key=" + record.key() + ",value=" + record.value() + ",partition="
+					+ record.partition() + ",offset=" + record.offset());
+		});
+
+		consumer.subscribe("jobs", ar -> {
+			if (ar.succeeded()) {
+				System.out.println("subscribed");
+			} else {
+				System.out.println("Could not subscribe " + ar.cause().getMessage());
+			}
+		});
+	}
 }
